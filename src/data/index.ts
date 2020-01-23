eaf4cbeeadf60257c497cc39aed8d55d81f45d6c
Alexander Drozdov <snosme@gmail.com>
2020-01-23 20:10:45 +0200

    update data

--- src/data/index.ts
+++ src/data/index.ts
@@ -2,18 +2,25 @@ import prophecies from './prophecies.json'
 import monsters from './itemised-monsters.json'
 import mods from './mods.json'
 
+export interface StatMatcher {
+  string: string
+  negate?: true
+  condition?: {
+    min?: number
+    max?: number
+  }
+  option?: {
+    text: string
+    tradeId: string | number
+  }
+}
+
 export interface Mod {
   text: string
   types: Array<{
     name: string
     tradeId?: string
-    // undefined when dupe mod found. Alternative impl is to store
-    // all tradeIds in array, and apply Logical OR with "min: 1 matched"
   }>
-  option?: {
-    text: string
-    tradeId: string | number
-  }
 }
 
 export const Prophecies = new Set(prophecies as string[])
