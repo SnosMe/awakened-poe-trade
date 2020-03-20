5b3cea94c1c09773ed324276c15179076d7591ff
Alexander Drozdov <snosme@gmail.com>
2020-03-20 15:06:57 +0200

    poe.ninja DeliriumOrb

--- src/components/Prices.ts
+++ src/components/Prices.ts
@@ -98,7 +98,8 @@ class PriceService {
       { overview: 'item', type: 'UniqueArmour', loaded: 0 },
       { overview: 'item', type: 'UniqueAccessory', loaded: 0 },
       { overview: 'item', type: 'Beast', loaded: 0 },
-      { overview: 'item', type: 'Vial', loaded: 0 }
+      { overview: 'item', type: 'Vial', loaded: 0 },
+      { overview: 'item', type: 'DeliriumOrb', loaded: 0 }
     ]
   })
 
