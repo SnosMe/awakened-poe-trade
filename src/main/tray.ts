18930f2b880259aa7dae36e4be299d0f1c8ea85b
Alexander Drozdov <snosme@gmail.com>
2020-03-24 17:00:25 +0200

    ico

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -40,7 +40,7 @@ function leaguesMenuItem () {
 }
 
 export function createTray () {
-  tray = new Tray(path.join(__static, 'icon.png')) // @TODO .ico for windows
+  tray = new Tray(path.join(__static, process.platform === 'win32' ? 'icon.ico' : 'icon.png'))
 
   ipcMain.on(LEAGUES_READY, (e, leagues_: League[]) => {
     leagues = leagues_
