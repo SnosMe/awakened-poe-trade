73c4c91d65fda511504f7652507e62c6f8af7304
Alexander Drozdov <snosme@gmail.com>
2020-03-25 22:22:32 +0200

    revert to png only

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -40,7 +40,7 @@ function leaguesMenuItem () {
 }
 
 export function createTray () {
-  tray = new Tray(path.join(__static, process.platform === 'win32' ? 'icon.ico' : 'icon.png'))
+  tray = new Tray(path.join(__static, 'icon.png'))
 
   ipcMain.on(LEAGUES_READY, (e, leagues_: League[]) => {
     leagues = leagues_
