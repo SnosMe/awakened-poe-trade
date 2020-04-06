86325151ea6500a1bb37a9128a25ad3ceed8ecf6
Alexander Drozdov <snosme@gmail.com>
2020-04-06 03:04:55 +0300

    fix native input validation

--- src/web/price-check/filters/FilterModifier.vue
+++ src/web/price-check/filters/FilterModifier.vue
@@ -12,10 +12,10 @@
         </div>
       </button>
       <div class="flex">
-        <ui-input-debounced class="search-num-input rounded-tl mr-px" placeholder="min" type="number" :class="{ 'rounded-bl': !showQ20Notice }"
+        <ui-input-debounced class="search-num-input rounded-tl mr-px" placeholder="min" step="any" type="number" :class="{ 'rounded-bl': !showQ20Notice }"
           v-if="showMinmaxInput" ref="inputMin"
           v-model.number="filter.min" @focus="inputFocus($event, 'min')" :delay="0" />
-        <ui-input-debounced class="search-num-input rounded-tr" placeholder="max" type="number" :class="{ 'rounded-br': !showQ20Notice }"
+        <ui-input-debounced class="search-num-input rounded-tr" placeholder="max" step="any" type="number" :class="{ 'rounded-br': !showQ20Notice }"
           v-if="showMinmaxInput" ref="inputMax"
           v-model.number="filter.max" @focus="inputFocus($event, 'max')" :delay="0" />
         <div v-if="filter.option"
