bccb47ab483f2e01999a75f91487c8e830b84aeb
nomis51 <nomis51@users.noreply.github.com>
2020-07-23 18:54:47 -0400

    Rework offer UI for better placement

--- src/web/trade-manager/incoming-offers/IncomingOffer.vue
+++ src/web/trade-manager/incoming-offers/IncomingOffer.vue
@@ -1,40 +1,53 @@
 <template>
-  <div class="incoming-offer bg-gray-800 mx-1 rounded">
-    <div class="bg-gray-900 incoming-offer-header" @click="sendParyInvite">
-      <ui-popper trigger="hover" :options="{ placement: 'top' }">
-        <template slot="reference">
-          <p class="text-gray-500">{{ offer.item | elipsis }}</p>
-        </template>
+  <div class="incoming-offer bg-gray-800 mx-1 rounded ripple">
+    <table style="width: 100%;">
+      <tr class="bg-gray-900 incoming-offer-header" @click="sendParyInvite">
+        <td colspan="2">
+          <ui-popper trigger="hover" :options="{ placement: 'top' }">
+            <template slot="reference">
+              <p class="text-gray-500">{{ offer.item | elipsis }}</p>
+            </template>
 
-        <div class="popper">
-          <p>{{ offer.item }}</p>
-        </div>
-      </ui-popper>
-    </div>
-    <div class="incoming-offer-content text-gray-500" @click="sendParyInvite">
-      <img
-        src="https://gamepedia.cursecdn.com/pathofexile_gamepedia/9/9c/Chaos_Orb_inventory_icon.png?version=e197f9fd3de58b1ebaa95ef9160af106"
-      />
-    </div>
-    <div class="incoming-offer-actions">
-      <div
-        class="incoming-offer-action-still-interested  incoming-offer-action"
-        @click="sendStillInterestedWhisper"
-      >
-        <i class="fas fa-question text-gray-500"></i>
-      </div>
-      <div
-        class="incoming-offer-action-dismiss incoming-offer-action"
-        @click="dismiss"
-      >
-        <i class="fas fa-times text-gray-500"></i>
-      </div>
-    </div>
+            <div class="popper">
+              <p>{{ offer.item }}</p>
+            </div>
+          </ui-popper>
+        </td>
+      </tr>
+      <tr class="incoming-offer-content text-gray-500" @click="sendParyInvite">
+        <td>
+          <img
+            src="https://gamepedia.cursecdn.com/pathofexile_gamepedia/9/9c/Chaos_Orb_inventory_icon.png?version=e197f9fd3de58b1ebaa95ef9160af106"
+          />
+        </td>
+        <td>
+          <p class="text-gray-500">50x</p>
+        </td>
+      </tr>
+      <tr class="incoming-offer-actions">
+        <td>
+          <div
+            class="incoming-offer-action-still-interested incoming-offer-action"
+            @click="sendStillInterestedWhisper"
+          >
+            <i class="fas fa-question text-gray-500"></i>
+          </div>
+        </td>
+        <td>
+          <div
+            class="incoming-offer-action-dismiss incoming-offer-action"
+            @click="dismiss"
+          >
+            <i class="fas fa-times text-gray-500"></i>
+          </div>
+        </td>
+      </tr>
+    </table>
   </div>
 </template>
 
 <script>
-import { MainProcess } from '../../../ipc/main-process-bindings';
+import { MainProcess } from "../../../ipc/main-process-bindings";
 export default {
   props: {
     offer: {
@@ -62,6 +75,7 @@ export default {
     sendParyInvite() {
       MainProcess.focusGame();
       this.$emit("partyInvite");
+      this.offer.partyInviteSent = true;
     },
     sendStillInterestedWhisper() {
       MainProcess.focusGame();
@@ -82,41 +96,25 @@ export default {
   pointer-events: all;
 }
 
+.incoming-offer > table {
+  border: 0.2rem solid #1a202c;
+}
+
+.incoming-offer > table > tr > td {
+  width: 50%;
+}
+
 .incoming-offer-header {
-  width: 100%;
-  height: 1.5rem;
   text-align: center;
-  border-top-left-radius: 0.25rem;
-  border-top-right-radius: 0.25rem;
 }
 
-.incoming-offer-content {
-  width: 100%;
+.incoming-offer-content > td > img {
+  height: 2rem;
   margin: auto;
 }
 
-.incoming-offer-content > img {
-  height: 2.5rem;
-  width: 2.5rem;
-  margin: auto;
-}
-
-.incoming-offer-actions {
-  height: 1rem;
-}
-
-.incoming-offer-action-still-interested {
-  height: 1rem;
-  width: 50%;
-  float: left;
-  border-bottom-left-radius: 0.25rem;
-}
-
-.incoming-offer-action-dismiss {
-  height: 1rem;
-  margin-left: 50%;
-  width: 50%;
-  border-bottom-right-radius: 0.25rem;
+.incoming-offer-content > td > p {
+  text-align: center;
 }
 
 .incoming-offer-action-still-interested:hover {
@@ -133,5 +131,28 @@ export default {
   left: 50%;
   top: 50%;
   transform: translateX(-50%) translateY(-95%);
+}
+
+.incoming-offer-action {
+  height: 1rem;
+}
+
+.incoming-offer-actions > td > span > span > .popper {
+  font-size: 0.6rem !important;
+}
+
+/* Ripple effect */
+.ripple {
+  background-position: center;
+  transition: background 0.8s;
+}
+.ripple:hover {
+  background: #2d3748 radial-gradient(circle, transparent 1%, #44526b 1%)
+    center/15000%;
+}
+.ripple:active {
+  background-color: #4b5a74;
+  background-size: 100%;
+  transition: background 0s;
 }
 </style>
