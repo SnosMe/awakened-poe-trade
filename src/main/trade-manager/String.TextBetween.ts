a74dfe9d2b45ee10bc12d75db5fcc1f163e6cb95
nomis51 <nomis51@users.noreply.github.com>
2020-07-25 19:52:11 -0400

    Add a use case to String.TextBetween

--- src/main/trade-manager/String.TextBetween.ts
+++ src/main/trade-manager/String.TextBetween.ts
@@ -2,7 +2,10 @@ interface String {
   textBetween: (start: string, end: string) => string;
 }
 
-String.prototype.textBetween = function(start: string, end: string): string {
+String.prototype.textBetween = function(
+  start: string,
+  end: string = ""
+): string {
   let startIndex = this.indexOf(start);
 
   if (startIndex < 0) {
@@ -11,11 +14,15 @@ String.prototype.textBetween = function(start: string, end: string): string {
 
   startIndex += start.length;
 
-  const endIndex = this.indexOf(end);
+  if (end) {
+    const endIndex = this.indexOf(end);
 
-  if (endIndex < 0) {
-    return "";
+    if (endIndex < 0) {
+      return "";
+    }
+
+    return this.substring(startIndex, endIndex);
+  } else {
+    return this.substring(startIndex);
   }
-
-  return this.substring(startIndex, endIndex);
 };
