c96b89425cf993c2831a6cd4885ae3a2ef84516f
Alexander Drozdov <snosme@gmail.com>
2020-12-15 19:43:03 +0200

    fix #257 #258

--- src/parser/Parser.ts
+++ src/parser/Parser.ts
@@ -330,8 +330,8 @@ function parseStackSize (section: string[], item: ParsedItem) {
     return PARSER_SKIPPED
   }
   if (section[0].startsWith(_$[C.TAG_STACK_SIZE])) {
-    // Portal Scroll "Stack Size: 2 448/40"
-    const [value, max] = section[0].substr(_$[C.TAG_STACK_SIZE].length).replace(/\u00a0/g, '').split('/').map(Number)
+    // Portal Scroll "Stack Size: 2[localized separator]448/40"
+    const [value, max] = section[0].substr(_$[C.TAG_STACK_SIZE].length).replace(/[^\d/]/g, '').split('/').map(Number)
     item.stackSize = { value, max }
 
     if (item.category === ItemCategory.Seed) {
