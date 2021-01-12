8b0c8657e8748fe99c98cc727b2c54ac7bba2e4c
Alexander Drozdov <snosme@gmail.com>
2021-01-12 23:28:44 +0200

    move

--- src/web/i18n.ts
+++ src/web/i18n.ts
@@ -1,14 +1,15 @@
-import { createI18n, VueI18nOptions } from 'vue-i18n'
+import { createI18n, I18nAdditionalOptions, ComposerOptions } from 'vue-i18n'
 import { Config } from './Config'
 
-export default createI18n<VueI18nOptions>({
+export default createI18n<I18nAdditionalOptions & ComposerOptions>({
+  legacy: false,
   locale: Config.store.language,
   fallbackLocale: 'en',
-  formatFallbackMessages: true,
-  silentFallbackWarn: true,
-  silentTranslationWarn: true,
+  fallbackFormat: true,
+  fallbackWarn: false,
+  missingWarn: false,
   messages: {
-    en: require('./locales/en.json'),
-    ru: require('./locales/ru.json')
+    en: require('../assets/locales/en.json'),
+    ru: require('../assets/locales/ru.json')
   }
 })
