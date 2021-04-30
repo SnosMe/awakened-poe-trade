49ce6ecc0b3519d783e2d81bf466c369c5aa3adc
Alexander Drozdov <snosme@gmail.com>
2021-04-30 19:21:47 +0300

    poe.ninja changes to maps

--- src/web/background/Prices.ts
+++ src/web/background/Prices.ts
@@ -86,6 +86,7 @@ const priceQueue = [
   { overview: 'item', type: 'SkillGem', loaded: 0 },
   { overview: 'item', type: 'BaseType', loaded: 0 },
   // { overview: 'item', type: 'HelmetEnchant', loaded: 0 },
+  { overview: 'item', type: 'BlightedMap', loaded: 0 },
   { overview: 'item', type: 'UniqueMap', loaded: 0 },
   { overview: 'item', type: 'Map', loaded: 0 },
   { overview: 'item', type: 'UniqueJewel', loaded: 0 },
