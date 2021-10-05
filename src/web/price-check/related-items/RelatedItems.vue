9268d1f6514f3996be971a9bd576c28a79bed607
DiscworldZA <pd.van.lill@gmail.com>
2021-10-05 02:18:59 +0200

    fix formatting

--- src/web/price-check/related-items/RelatedItems.vue
+++ src/web/price-check/related-items/RelatedItems.vue
@@ -1,49 +1,39 @@
 <template>
-<div v-if="result" class="flex max-w-full bg-gray-800 text-gray-400 mt-6 border border-gray-900"
-     style="border-width: 0.25rem;">
-  <div class="flex-1 p-2 w-1/2">
-    <div v-for="item in result.related" :key="item.detailsId"
-         :class="{ 'bg-gray-700 -mx-1 px-1': item.detailsId === detailsId }" class="rounded">
-      <div class="flex items-center flex-1 px-2">
-        <div class="w-8 h-8 flex items-center justify-center flex-shrink-0">
-          <img :src="item.icon" :alt="item.name" class="max-w-full max-h-full">
-        </div>
-        <template v-if="!isPrivateLeague">
+  <div v-if="result" class="flex max-w-full bg-gray-800 text-gray-400 mt-6 border border-gray-900" style="border-width: 0.25rem;">
+    <div class="flex-1 p-2 w-1/2">
+      <div v-for="item in result.related" :key="item.detailsId"
+        :class="{ 'bg-gray-700 -mx-1 px-1': item.detailsId === detailsId }" class="rounded">
+        <div class="flex items-center flex-1">
+          <div class="w-8 h-8 flex items-center justify-center flex-shrink-0">
+            <img :src="item.icon" :alt="item.name" class="max-w-full max-h-full">
+          </div>
           <i class="fas fa-arrow-right text-gray-600 px-2"></i>
-          <span class="px-1 text-base whitespace-no-wrap overflow-hidden">{{
-              price(item).val
-            }} {{ price(item).curr === 'e' ? 'exa' : 'chaos' }}</span>
-        </template>
+          <span class="px-1 text-base whitespace-no-wrap overflow-hidden">{{ price(item).val }} {{ price(item).curr === 'e' ? 'exa' : 'chaos' }}</span>
+        </div>
+        <div class="text-left text-gray-600 mb-1 whitespace-no-wrap overflow-hidden">{{ item.name }}</div>
       </div>
-      <div class="text-left text-gray-600 mb-1 whitespace-no-wrap overflow-hidden">{{ item.name }}</div>
+    </div>
+    <div class="flex-1 p-2 w-1/2" v-if="result.items.length">
+      <div v-for="item in result.items" :key="item.detailsId">
+        <div class="flex items-center flex-1">
+          <div class="w-8 h-8 flex items-center justify-center flex-shrink-0">
+            <img :src="item.icon" :alt="item.name" class="max-w-full max-h-full">
+          </div>
+          <i class="fas fa-arrow-right text-gray-600 px-2"></i>
+          <span class="px-1 text-base whitespace-no-wrap overflow-hidden">{{ price(item).val }} {{ price(item).curr === 'e' ? 'exa' : 'chaos' }}</span>
+        </div>
+        <div class="text-left text-gray-600 mb-1 whitespace-no-wrap overflow-hidden">{{ item.name }}</div>
+      </div>
     </div>
   </div>
-  <div class="flex-1 p-2 w-1/2" v-if="result.items.length">
-    <div v-for="item in result.items" :key="item.detailsId">
-      <div class="flex items-center flex-1 px-2">
-        <div class="w-8 h-8 flex items-center justify-center flex-shrink-0">
-          <img :src="item.icon" :alt="item.name" class="max-w-full max-h-full">
-        </div>
-        <template v-if="!isPrivateLeague">
-          <i class="fas fa-arrow-right text-gray-600 px-2"></i>
-          <span class="px-1 text-base whitespace-no-wrap overflow-hidden">{{
-              price(item).val
-            }} {{ price(item).curr === 'e' ? 'exa' : 'chaos' }}</span>
-        </template>
-      </div>
-      <div class="text-left text-gray-600 mb-1 whitespace-no-wrap overflow-hidden">{{ item.name }}</div>
-    </div>
-  </div>
-</div>
 </template>
 
 <script lang="ts">
 import { computed, defineComponent, PropType } from 'vue'
 import { ITEM_DROP } from '@/assets/data'
-import { autoCurrency, displayRounding, findByDetailsId, ItemInfo } from '../../background/Prices'
+import { displayRounding, ItemInfo, findByDetailsId, autoCurrency } from '../../background/Prices'
 import { getDetailsId } from '../trends/getDetailsId'
 import { ParsedItem } from '@/parser'
-import { AppConfig } from '@/web/Config'
 
 export default defineComponent({
   props: {
@@ -73,7 +63,6 @@ export default defineComponent({
     })
 
     return {
-      isPrivateLeague: AppConfig().isPrivateLeague,
       result,
       detailsId,
       price (item: ItemInfo) {
