f0d586a1d850f47487d06822e9795f0b9cfd65a9
DiscworldZA <pd.van.lill@gmail.com>
2021-10-09 00:05:05 +0200

    sigh

--- src/web/settings/price-check.vue
+++ src/web/settings/price-check.vue
@@ -38,7 +38,7 @@
     <div class="mb-2">
       <div class="flex-1 mb-1">{{ t('Account name') }}</div>
       <div class="mb-4">
-        <input v-model="accountName" class="rounded bg-gray-900 px-1 block w-full mb-1 font-fontin-regular"/>
+        <input v-model="accountName" class="rounded bg-gray-900 px-1 block w-full mb-1 font-fontin-regular" />
       </div>
     </div>
     <div class="mb-2">
@@ -65,13 +65,12 @@
       <div class="flex-1 mb-1">{{ t('Minimum buyout price') }}</div>
       <div class="mb-4 flex">
         <div class="flex mr-6">
-          <input v-model.number="chaosPriceThreshold"
-                 class="rounded bg-gray-900 px-1 block w-16 mb-1 font-fontin-regular text-center"/>
+          <input v-model.number="chaosPriceThreshold" class="rounded bg-gray-900 px-1 block w-16 mb-1 font-fontin-regular text-center" />
           <span class="ml-2">{{ t('Chaos Orbs') }}</span>
         </div>
       </div>
     </div>
-    <div class="mb-2">
+    <div class="mb-2" >
       <div class="flex-1 mb-1">{{ t('Always select "Stock" filter') }}</div>
       <div class="mb-4 flex">
         <ui-radio v-model="activateStockFilter" :value="true" class="mr-4">{{ t('Yes') }}</ui-radio>
