40826ed2d681b0b3ce32015e23c2b32580cc93e2
Alexander Drozdov <snosme@gmail.com>
2022-12-05 05:28:08 +0200

    add image

--- renderer/src/web/settings/SettingsWindow.vue
+++ renderer/src/web/settings/SettingsWindow.vue
@@ -17,7 +17,10 @@
           <div v-else
             class="border-b mx-2 border-gray-800" />
         </template>
-        <div class="text-gray-400 text-center mt-auto pr-3 pt-4 pb-12" style="max-width: fit-content; min-width: 100%;">{{ t('Support development on') }}<br> <a href="https://patreon.com/awakened_poe_trade" class="inline-flex mt-1" target="_blank"><img class="inline h-5" src="/images/Patreon.svg"></a></div>
+        <div class="text-gray-400 text-center mt-auto pr-3 pt-4 pb-12" style="max-width: fit-content; min-width: 100%;">
+          <img class="mx-auto mb-1" src="/images/peepoLove2x.webp">
+          {{ t('Support development on') }}<br> <a href="https://patreon.com/awakened_poe_trade" class="inline-flex mt-1" target="_blank"><img class="inline h-5" src="/images/Patreon.svg"></a>
+        </div>
       </div>
       <div class="text-gray-100 grow layout-column bg-gray-900">
         <div class="grow overflow-y-auto bg-gray-800 rounded-tl">
