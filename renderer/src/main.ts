deb947e65e7e02a154898389a792c65e84042771
Alexander Drozdov <snosme@gmail.com>
2023-01-15 21:12:30 +0200

    fix random error on connecting socket #854

--- renderer/src/main.ts
+++ renderer/src/main.ts
@@ -4,11 +4,13 @@ import * as I18n from './web/i18n'
 import * as Data from './assets/data'
 import uiComponents from './web/ui'
 import { initConfig, AppConfig } from './web/Config'
+import { Host } from './web/background/IPC'
 
 ;(async function () {
   await initConfig()
   const i18nPlugin = await I18n.init(AppConfig().language)
   await Data.init(AppConfig().language)
+  await Host.init()
 
   watch(() => AppConfig().language, async () => {
     await Data.loadForLang(AppConfig().language)
