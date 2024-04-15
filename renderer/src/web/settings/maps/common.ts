d3233594908dd7028076d3ea9a5d0f38557b93a9
Alexander Drozdov <snosme@gmail.com>
2024-04-15 11:02:06 +0300

    refactor map-check buttons

---
+++ renderer/src/web/settings/maps/common.ts
@@ -0,0 +1,37 @@
+export enum StatTag {
+  Outdated = 1,
+  HeistExclusive,
+  UberMapExclusive
+}
+
+export interface StatMatcher {
+  matchStr: string
+  tag?: StatTag
+}
+
+export function decisionHasColor (decisionSet: string, profileName: number) {
+  // Three profiles stored as "---"
+  // -: undefined
+  // d: deadly mod
+  // w: warning
+  // g: good mod
+  // s: marked as seen
+  const idx = profileName - 1
+  return decisionSet[idx] !== '-' && decisionSet[idx] !== 's'
+}
+
+export function nextDecision (current: string): string {
+  switch (current) {
+    case 'd': return 'w'
+    case 'w': return 'g'
+    case 'g': return 's'
+  }
+  return 'd'
+}
+
+export function decisionCreate (value: string, profileName: number, updateSet = '---') {
+  const idx = profileName - 1
+  const split = updateSet.split('')
+  split[idx] = value
+  return split.join('')
+}
