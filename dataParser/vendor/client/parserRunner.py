099bb1a9adae36d79c8bcba209296dd3e53220df
kvan7 <kvan.valve@gmail.com>
2025-05-30 18:40:40 -0500

    add deprecation notice to parser

--- dataParser/vendor/client/parserRunner.py
+++ dataParser/vendor/client/parserRunner.py
@@ -9,7 +9,14 @@ SUPPORTED_LANGUAGES = ["en", "ru", "ko", "cmn-Hant", "ja", "de", "es"]
 if __name__ == "__main__":
     logger.info("Starting parser")
     set_log_level(logging.WARNING)
+    logger.critical(
+        "This parser is deprecated and is not being maintained. It may not work as expected."
+    )
     for lang in SUPPORTED_LANGUAGES:
         logger.info(f"Generating {lang} tables")
         parser = Parser(lang)
         parser.run()
+
+    logger.critical(
+        "This parser is deprecated and is not being maintained. It may not work as expected."
+    )
