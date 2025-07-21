6222248de4d96b0bf741c966cc4df15d5b59bc2f
luan.nguyen <luan.nguyen@parcelperform.com>
2025-07-21 17:30:02 +0700

    Trigger workflows

--- main/src/main.ts
+++ main/src/main.ts
@@ -1,61 +1,73 @@
-'use strict'
+"use strict";
 
-import { app } from 'electron'
-import { uIOhook } from 'uiohook-napi'
-import os from 'node:os'
-import { startServer, eventPipe, server } from './server'
-import { Logger } from './RemoteLogger'
-import { GameWindow } from './windowing/GameWindow'
-import { OverlayWindow } from './windowing/OverlayWindow'
-import { GameConfig } from './host-files/GameConfig'
-import { Shortcuts } from './shortcuts/Shortcuts'
-import { AppUpdater } from './AppUpdater'
-import { AppTray } from './AppTray'
-import { OverlayVisibility } from './windowing/OverlayVisibility'
-import { GameLogWatcher } from './host-files/GameLogWatcher'
-import { HttpProxy } from './proxy'
+import { app } from "electron";
+import { uIOhook } from "uiohook-napi";
+import os from "node:os";
+import { startServer, eventPipe, server } from "./server";
+import { Logger } from "./RemoteLogger";
+import { GameWindow } from "./windowing/GameWindow";
+import { OverlayWindow } from "./windowing/OverlayWindow";
+import { GameConfig } from "./host-files/GameConfig";
+import { Shortcuts } from "./shortcuts/Shortcuts";
+import { AppUpdater } from "./AppUpdater";
+import { AppTray } from "./AppTray";
+import { OverlayVisibility } from "./windowing/OverlayVisibility";
+import { GameLogWatcher } from "./host-files/GameLogWatcher";
+import { HttpProxy } from "./proxy";
 
 if (!app.requestSingleInstanceLock()) {
-  app.exit()
+  app.exit();
 }
 
-if (process.platform !== 'darwin') {
-  app.disableHardwareAcceleration()
+if (process.platform !== "darwin") {
+  app.disableHardwareAcceleration();
 }
-app.enableSandbox()
+app.enableSandbox();
 
-let tray: AppTray
+let tray: AppTray;
 
-app.on('ready', async () => {
-  tray = new AppTray(eventPipe)
-  const logger = new Logger(eventPipe)
-  const gameLogWatcher = new GameLogWatcher(eventPipe, logger)
-  const gameConfig = new GameConfig(eventPipe, logger)
-  const poeWindow = new GameWindow()
-  const appUpdater = new AppUpdater(eventPipe)
-  const _httpProxy = new HttpProxy(server, logger)
+app.on("ready", async () => {
+  tray = new AppTray(eventPipe);
+  const logger = new Logger(eventPipe);
+  const gameLogWatcher = new GameLogWatcher(eventPipe, logger);
+  const gameConfig = new GameConfig(eventPipe, logger);
+  const poeWindow = new GameWindow();
+  const appUpdater = new AppUpdater(eventPipe);
+  const _httpProxy = new HttpProxy(server, logger);
 
   setTimeout(
     async () => {
-      const overlay = new OverlayWindow(eventPipe, logger, poeWindow)
-      new OverlayVisibility(eventPipe, overlay, gameConfig)
-      const shortcuts = await Shortcuts.create(logger, overlay, poeWindow, gameConfig, eventPipe)
-      eventPipe.onEventAnyClient('CLIENT->MAIN::update-host-config', (cfg) => {
-        overlay.updateOpts(cfg.overlayKey, cfg.windowTitle)
-        shortcuts.updateActions(cfg.shortcuts, cfg.stashScroll, cfg.logKeys, cfg.restoreClipboard, cfg.language)
-        gameLogWatcher.restart(cfg.clientLog ?? '')
-        gameConfig.readConfig(cfg.gameConfig ?? '')
-        appUpdater.checkAtStartup()
-        tray.overlayKey = cfg.overlayKey
-      })
-      uIOhook.start()
-      const port = await startServer(appUpdater, logger)
+      const overlay = new OverlayWindow(eventPipe, logger, poeWindow);
+      new OverlayVisibility(eventPipe, overlay, gameConfig);
+      const shortcuts = await Shortcuts.create(
+        logger,
+        overlay,
+        poeWindow,
+        gameConfig,
+        eventPipe
+      );
+      eventPipe.onEventAnyClient("CLIENT->MAIN::update-host-config", (cfg) => {
+        overlay.updateOpts(cfg.overlayKey, cfg.windowTitle);
+        shortcuts.updateActions(
+          cfg.shortcuts,
+          cfg.stashScroll,
+          cfg.logKeys,
+          cfg.restoreClipboard,
+          cfg.language
+        );
+        gameLogWatcher.restart(cfg.clientLog ?? "");
+        gameConfig.readConfig(cfg.gameConfig ?? "");
+        appUpdater.checkAtStartup();
+        tray.overlayKey = cfg.overlayKey;
+      });
+      uIOhook.start();
+      const port = await startServer(appUpdater, logger);
       // TODO: move up (currently crashes)
-      logger.write(`info ${os.type()} ${os.release} / v${app.getVersion()}`)
-      overlay.loadAppPage(port)
-      tray.serverPort = port
+      logger.write(`info ${os.type()} ${os.release} / v${app.getVersion()}`);
+      overlay.loadAppPage(port);
+      tray.serverPort = port;
     },
     // fixes(linux): window is black instead of transparent
-    process.platform === 'linux' ? 1000 : 0
-  )
-})
+    process.platform === "linux" ? 1000 : 0
+  );
+});
