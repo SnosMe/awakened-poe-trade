5202883daf032922176f52bc371daca527315b52
Alexander Drozdov <snosme@gmail.com>
2026-07-25 15:56:12 +0300

    update app version on site

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -20,7 +20,7 @@ export default defineConfig({
   },
   themeConfig: {
     // logo: 'TODO', https://github.com/vuejs/vitepress/issues/1401
-    appVersion: '3.28.104',
+    appVersion: '3.29.101',
     github: {
       releasesUrl: 'https://github.com/SnosMe/awakened-poe-trade/releases'
     },
