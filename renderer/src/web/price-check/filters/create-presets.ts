0c2e3b3671f6025545feb31ebc8d33a3ab54d1f7
Alexander Drozdov <snosme@gmail.com>
2026-08-17 01:43:39 +0300

    fix accessing info that's only available on Charts

--- renderer/src/web/price-check/filters/create-presets.ts
+++ renderer/src/web/price-check/filters/create-presets.ts
@@ -71,7 +71,9 @@ export function createPresets (
           stats: createExactStatFilters(item, item.statsByType, { ...opts, mode: 'props' })
         }
         return {
-          active: (item.mapArea!.area!.special) ? bulkPreset.id : propsPreset.id,
+          active: (item.category === ItemCategory.Chart && item.mapArea!.area!.special)
+            ? bulkPreset.id
+            : propsPreset.id,
           presets: [propsPreset, bulkPreset]
         }
       } else {
